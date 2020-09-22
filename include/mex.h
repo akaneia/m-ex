5a1a03ea3372d64b22cdd263df51db961e78c31c
UnclePunch <vinbertinelli@gmail.com>
2020-09-21 21:24:49 -0400

    wolf: start on kb items

--- include/mex.h
+++ include/mex.h
@@ -31,6 +31,8 @@ struct MEXPlaylist
 void (*MEX_IndexFighterItem)(GOBJ *player_gobj, int article_pointer, int item_id) = (void *)0x803d7058;
 void (*SpawnMEXEffect)(int effectID, int fighter, int arg1, int arg2, int arg3, int arg4, int arg5) = (void *)0x803d7060;
 int (*MEX_GetItemExtID)(GOBJ *gobj, int item_id) = (void *)0x803d7064; // gobj can be fighter or stage
+int (*MEX_GetFtItemID)(int ft_kind, int item_id) = (void *)0x803d7088; // gobj can be fighter or stage
+int (*MEX_GetGrItemExtID)(int item_id) = (void *)0x803d708C;           // gobj can be fighter or stage
 void (*SFX_PlayStageSFX)(int sfx_id) = (void *)0x803d7078;
 void *(*calloc)(int size) = (void *)0x803d706C;
 PRIM *(*PRIM_NEW)(int vert_count, int params1, int params2) = (void *)0x804DD84C;
@@ -38,5 +40,6 @@ void (*PRIM_CLOSE)() = (void *)0x804DD848;
 MEXPlaylist *(*MEX_GetPlaylist)() = (void *)0x803d707C;
 int (*MEX_GetStageItemExtID)(int item_id) = (void *)0x803d7064;
 void (*KirbyStateChange)(GOBJ *fighter, int state, float startFrame, float animSpeed, float animBlend) = 0x803d7080;
-void *(*MEX_GetKirbyCpData)(int ability_id) = 0x803d7084;
+void *(*MEX_GetKirbyCpData)(int copy_id) = 0x803d7084;
+int (*MEX_GetCopyItemExtID)(int copy_id, int item_id) = 0x803d7088;
 #endif
