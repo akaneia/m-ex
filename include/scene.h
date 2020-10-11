308b4196aeba0eee11db004ab721ae00a5213612
UnclePunch <vinbertinelli@gmail.com>
2020-10-11 18:54:26 -0400

    scenes: add minor overriding

--- include/scene.h
+++ include/scene.h
@@ -16,51 +16,53 @@ enum HEAP_KIND
 };
 enum MINOR_KIND
 {
-    MNRKIND_TITLE,     // Title Screen
-    MNRKIND_MNMA,      // Main Menu
-    MNRKIND_MATCH,     // VS Dairantou (In-Game)
-    MNRKIND_SUDDEATH,  // Sudden Death Dairantou (In-Game)
-    MNRKIND_TRAIN,     // Training Mode Dairantou (In-Game)
-    MNRKIND_RST,       // Result Screen
-    MNRKIND_X6,        //
-    MNRKIND_DB,        // Debug Menu
-    MNRKIND_CSS,       // CSS
-    MNRKIND_SSS,       // SSS
-    MNRKIND_XA,        //
-    MNRKIND_TYGAL,     // Trophy Gallery
-    MNRKIND_TYLOT,     // Trophy Lottery
-    MNRKIND_TYCOL,     // Trophy Collection
-    MNRKIND_ADVSPLSH,  // Adventure Mode Splash Screen
-    MNRKIND_TYFALL,    // 1P Mode Trophy Falling Cutscene
-    MNRKIND_ADVCGRT,   // Adventure Mode Congratulations
-    MNRKIND_VI1,       // VisualScene_Luigi
-    MNRKIND_VI2,       // VisualScene_BrinstarLava
-    MNRKIND_VI3,       // VisualScene_PlanetExplode
-    MNRKIND_VI4,       // VisualScene_3KirbysSpawn
-    MNRKIND_VI5,       // VisualScene_GiantKirbySpawns
-    MNRKIND_VI6,       // VisualScene_StarFoxDialog
-    MNRKIND_VI7,       // VisualScene_FZeroRace
-    MNRKIND_VI8,       // VisualScene_MetalMarioLuigi
-    MNRKIND_VI9,       // VisualScene_BowserTrophyFalls
-    MNRKIND_VI10,      // VisualScene_GigaBowserTransformation
-    MNRKIND_VI11,      // VisualScene_GigaBowserDefeated
-    MNRKIND_OP,        // Opening Movie
-    MNRKIND_1PENDMV,   // 1P Mode End Movie
-    MNRKIND_HOWMV,     // How to Play Movie
-    MNRKIND_OMAKE,     // Special Movie
-    MNRKIND_CLSCSPLSH, // TEST
-    MNRKIND_ALSPLSH,   // TEST
-    MNRKIND_GMOV,      // Game over
-    MNRKIND_SOON,      // coming soon
-    MNRKIND_TOSETUP,   // TEST
-    MNRKIND_TOBRCK,    // TEST
-    MNRKIND_TOUNK,     // TEST
-    MNRKIND_SPCLMSG,   // TEST
-    MNRKIND_PROG,      // TEST
-    MNRKIND_CHLG,      // TEST
-    MNRKIND_CARD,      // TEST
-    MNRKIND_STAFF,     // TEST
-    MNRKIND_CAMWARN,   // TEST
+    MNRKIND_TITLE,      // Title Screen
+    MNRKIND_MNMA,       // Main Menu
+    MNRKIND_MATCH,      // VS Dairantou (In-Game)
+    MNRKIND_SUDDEATH,   // Sudden Death Dairantou (In-Game)
+    MNRKIND_TRAIN,      // Training Mode Dairantou (In-Game)
+    MNRKIND_RST,        // Result Screen
+    MNRKIND_X6,         //
+    MNRKIND_DB,         // Debug Menu
+    MNRKIND_CSS,        // CSS
+    MNRKIND_SSS,        // SSS
+    MNRKIND_XA,         //
+    MNRKIND_TYGAL,      // Trophy Gallery
+    MNRKIND_TYLOT,      // Trophy Lottery
+    MNRKIND_TYCOL,      // Trophy Collection
+    MNRKIND_ADVSPLSH,   // Adventure Mode Splash Screen
+    MNRKIND_TYFALL,     // 1P Mode Trophy Falling Cutscene
+    MNRKIND_ADVCGRT,    // Adventure Mode Congratulations
+    MNRKIND_VI1,        // VisualScene_Luigi
+    MNRKIND_VI2,        // VisualScene_BrinstarLava
+    MNRKIND_VI3,        // VisualScene_PlanetExplode
+    MNRKIND_VI4,        // VisualScene_3KirbysSpawn
+    MNRKIND_VI5,        // VisualScene_GiantKirbySpawns
+    MNRKIND_VI6,        // VisualScene_StarFoxDialog
+    MNRKIND_VI7,        // VisualScene_FZeroRace
+    MNRKIND_VI8,        // VisualScene_MetalMarioLuigi
+    MNRKIND_VI9,        // VisualScene_BowserTrophyFalls
+    MNRKIND_VI10,       // VisualScene_GigaBowserTransformation
+    MNRKIND_VI11,       // VisualScene_GigaBowserDefeated
+    MNRKIND_OP,         // Opening Movie
+    MNRKIND_1PENDMV,    // 1P Mode End Movie
+    MNRKIND_HOWMV,      // How to Play Movie
+    MNRKIND_OMAKE,      // Special Movie
+    MNRKIND_CLSCSPLSH,  // TEST
+    MNRKIND_ALSPLSH,    // TEST
+    MNRKIND_GMOV,       // Game over
+    MNRKIND_SOON,       // coming soon
+    MNRKIND_TOSETUP,    // tournament setup
+    MNRKIND_TOBRCK,     // tourn bracket
+    MNRKIND_TOUNK,      // tourn unk
+    MNRKIND_SPCLMSG,    // special msg
+    MNRKIND_PROG,       // progressive
+    MNRKIND_CHLG,       // challenger
+    MNRKIND_CARD,       // memcard prompt
+    MNRKIND_STAFF,      // credits
+    MNRKIND_CAMWARN,    // camera mode memcard prompt
+    MNRKIND_NULL,       // terminator
+    MNRKIND_SMSHDWNCSS, // custom smashdown css
 };
 
 struct MajorScene
