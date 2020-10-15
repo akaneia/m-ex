af26f4b8d4e9b6a0ea4171359558859100da7f1b
UnclePunch <vinbertinelli@gmail.com>
2020-10-15 13:29:59 -0400

    mex: fix resizing of ifall heap

--- m-ex/Persistent Heap Expansion/Relocate Heap Def.asm
+++ m-ex/Persistent Heap Expansion/Relocate Heap Def.asm
@@ -2,10 +2,29 @@
 .include "../../Globals.s"
 .include "../Header.s"
 
-bl  HeapSetup
-mflr    r0
-b   Exit
+# get file size for ifall.usd
+    bl  FileName
+    mflr r3
+    branchl r12,0x800163d8
 
+# add to base value
+    load r4,0x41C644
+    add r3,r3,r4
+
+# get persistent heap setup table
+    bl  HeapSetup
+    mflr r4
+    stw r3, (1 * 0x10) + 0xC (r4)
+
+
+# Exit
+    mr    r0,r4
+    b   Exit
+
+FileName:
+blrl
+.string "IfAll.usd"
+.align 2
 
 HeapSetup:
 blrl
