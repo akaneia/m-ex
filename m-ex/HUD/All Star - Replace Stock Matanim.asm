c63b6f1006933265f20ea135621ada66362c5438
UnclePunch <vinbertinelli@gmail.com>
2020-10-18 19:54:13 -0400

    all star: fix stock icons

---
+++ m-ex/HUD/All Star - Replace Stock Matanim.asm
@@ -0,0 +1,33 @@
+#To be inserted @ 802f9764
+.include "../../Globals.s"
+.include "../Header.s"
+ 
+.set REG_Count,31
+.set REG_MatAnimJoint,20
+.set REG_StockJOBJ,30
+
+backup
+
+# check if exists
+  lwz r3,OFST_stc_icons(r13)
+  cmpwi r3,0
+  beq Exit
+  lwz REG_MatAnimJoint,0x4(r3)
+
+# Get stock icon jobj
+  mr  r3,REG_StockJOBJ
+  addi  r4,sp,0x80
+  li    r5,1
+  li  r6,-1
+  branchl r12,0x80011e24
+
+# add anim
+  lwz r3,0x80(sp)
+  li  r4,0
+  mr  r5,REG_MatAnimJoint
+  li  r6,0
+  branchl r12,0x8036fb5c
+
+Exit:
+restore
+addi	r3, r30, 0
