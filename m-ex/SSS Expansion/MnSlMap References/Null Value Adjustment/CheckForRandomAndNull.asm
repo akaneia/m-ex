b33f76c9183206d0539368134ce0dde4be95ed32
UnclePunch <vinbertinelli@gmail.com>
2020-10-19 01:08:37 -0400

    sss: hide model preview for id -1

--- m-ex/SSS Expansion/MnSlMap References/Null Value Adjustment/CheckForRandomAndNull.asm
+++ m-ex/SSS Expansion/MnSlMap References/Null Value Adjustment/CheckForRandomAndNull.asm
@@ -10,8 +10,17 @@
   subi  r12,r12,1
   cmpw r3,r12
   beq NoModelPreview
+# Check for hidden preview ID
+  mulli r0,r3,SSS_Stride
+  add r3,r31,r0
+  lbz r3,0x9(r3)
+  extsb r3,r3
+  cmpwi r3,-1
+  beq NoModelPreview
+
   b Exit
 NoModelPreview:
   branch  r12,0x8025a1d8
 
 Exit:
+  lwz	r3, 0 (r30)
