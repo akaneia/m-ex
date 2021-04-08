13345ac24ef67a30325ed06dddade9a4779ac75f
UnclePunch <vinbertinelli@gmail.com>
2021-04-07 23:32:34 -0400

    Update audio.h

--- MexTK/include/audio.h
+++ MexTK/include/audio.h
@@ -8,6 +8,7 @@ char *Nametag_GetText(int tag_index);
 void Audio_ResetCache(int group_index);
 void Audio_QueueFileLoad(int group_index, u64 ssm_index);
 void Audio_UpdateCache();
+void Audio_RequestSSMLoad(int ssm_id);
 void Audio_SyncLoadAll();
 int BGM_GetMenuBGM();
 void BGM_Play(int hpsID);
