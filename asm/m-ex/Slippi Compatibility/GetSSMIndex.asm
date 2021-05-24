c876eaddd02205b7248699de79f9ba2fda811aa2
UnclePunch <vinbertinelli@gmail.com>
2021-05-24 01:00:34 -0400

    slippi: fix FGM crash hopefully

---
+++ asm/m-ex/Slippi Compatibility/GetSSMIndex.asm
@@ -0,0 +1,35 @@
+#To be inserted @ 800056a4
+.include "../../Globals.s"
+.include "../Header.s"
+
+################################################################################
+# Address: FN_GetSSMIndex
+################################################################################
+# Inputs:
+# r3: kind (fighter / stage)
+# r4: index
+################################################################################
+# Returns
+# r3: SSM ID
+################################################################################
+# Description:
+# Returns SSM ID for inputted fighter/stage ID
+################################################################################
+
+cmpwi r3,1
+beq isStage
+
+isFighter:
+  lwz  r3,OFST_MnSlChrSSMFileIDs(rtoc)
+  mulli r4,r4,0x10
+  lbzx r3,r3,r4
+  b Exit 
+
+isStage:
+  lwz r3,OFST_Map_Audio(rtoc)
+  mulli r4,r4,0x3
+  lbzx r3,r3,r4
+  b Exit 
+
+Exit:
+blr
