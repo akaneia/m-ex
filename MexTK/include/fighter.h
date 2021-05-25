5d990da43fca855c7532ecb13b114f33341e0302
UnclePunch <vinbertinelli@gmail.com>
2021-05-25 03:11:03 -0400

    guess what, more fighter stuff

--- MexTK/include/fighter.h
+++ MexTK/include/fighter.h
@@ -732,10 +732,10 @@ struct FtCmd
 {
     char *anim_symbol; // ptr to the symbol name for this animation
     int anim_offset;   // offset of the anim data in plxxaj
-    void *anim_data;   // pointer to the animation data in ARAM
     void *anim_size;   // size of the animation data
     void *script;      // pointer to the script data for this action
     int flags;
+    void *anim_data; // pointer to the animation data in ARAM
 };
 
 struct ftData
@@ -3001,5 +3001,9 @@ void Fighter_ReleaseItemUnk(int ply, int ms, GOBJ *item);
 void Fighter_InitDamageVibrate(FighterData *fp, int dmg, float mult, int current_state, int unk_bool);
 float Fighter_CalcHitlagFrames(int dmg, int state_id, float mult);
 void Figher_LoadAnimation(GOBJ *f, GOBJ *anim_source, int anim_id);
-void Figher_ApplyAnimation(GOBJ *f, float start_frame, float speed, float blend);)
+void Figher_ApplyAnimation(GOBJ *f, float start_frame, float speed, float blend);
+void Fighter_UpdateStateFrameInfo(GOBJ *f);
+void Fighter_ScriptUpdate(GOBJ *f);
+void Fighter_ScriptFastForward(GOBJ *f);
+
 #endif
