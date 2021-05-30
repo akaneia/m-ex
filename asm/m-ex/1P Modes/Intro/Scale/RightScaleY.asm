467fb3a75c6bae72112c335d99f73dfe6b00f432
UnclePunch <vinbertinelli@gmail.com>
2021-05-30 04:16:02 -0400

    intro scale from mxdt

---
+++ asm/m-ex/1P Modes/Intro/Scale/RightScaleY.asm
@@ -0,0 +1,21 @@
+#To be inserted @ 8018470c
+.include "../../../../Globals.s"
+.include "../../../Header.s"
+
+# original
+  lfs	f1, 0x0078 (r4)
+
+# check for m-ex fighter
+  lbz r11,0x0(r25)
+  cmpwi r11,0x1A
+  blt Exit
+
+# get scale override
+  lwz r12,OFST_mexData(rtoc)
+  lwz r12,Arch_Fighter(r12)
+  lwz r12,Arch_Fighter_GmRst_Scale(r12)
+  mulli r11, r11, 4
+  lfsx f1,r11,r12
+
+Exit:
+  
---
+++ asm/m-ex/1P Modes/Intro/Scale/RightScaleZ.asm
@@ -0,0 +1,21 @@
+#To be inserted @ 801847a8
+.include "../../../../Globals.s"
+.include "../../../Header.s"
+
+# original
+  lfs	f1, 0x007C (r4)
+
+# check for m-ex fighter
+  lbz r11,0x0(r25)
+  cmpwi r11,0x1A
+  blt Exit
+
+# get scale override
+  lwz r12,OFST_mexData(rtoc)
+  lwz r12,Arch_Fighter(r12)
+  lwz r12,Arch_Fighter_GmRst_Scale(r12)
+  mulli r11, r11, 4
+  lfsx f1,r11,r12
+
+Exit:
+  
