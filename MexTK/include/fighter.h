84bb54c14032a29a3c3ea0efb02bc8e06538af80
UnclePunch <vinbertinelli@gmail.com>
2021-06-17 07:44:57 -0400

    Update fighter.h

--- MexTK/include/fighter.h
+++ MexTK/include/fighter.h
@@ -2960,7 +2960,7 @@ void Fighter_PhysAir_ApplyGravityFastfall(GOBJ *);
 void Fighter_PhysAir_ApplyGravity(FighterData *fighter, float gravity, float limit);
 void Fighter_PhysAir_ApplyAerialDrift(FighterData *fighter);
 void Fighter_PhysAir_ApplyCustomAerialDrift(FighterData *fighter, float unk, float speed, float limit);
-void Fighter_PhysAir_SetAerialDrift(FighterData *fp, float curr_x_vel, float this_frame_drift, float x_vel_limit, float aerial_friction);
+void Fighter_PhysAir_SetAerialDrift(FighterData *fp, float curr_x_vel, float this_frame_drift, float x_vel_limit, float aerial_friction); // this func name sucks but i just need to link it lol
 void Fighter_PhysAir_DecayXVelocity(FighterData *fighter, float aerial_friction);
 void Fighter_PhysAir_LimitXVelocity(FighterData *fighter);
 void Fighter_Phys_UseAnimYVelocity(GOBJ *fighter);
