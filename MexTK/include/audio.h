e75c00939cb25a98b53a6a9faac11f9573ea18b9
UnclePunch <vinbertinelli@gmail.com>
2021-09-18 20:03:56 -0400

    audio: add fgm groups

--- MexTK/include/audio.h
+++ MexTK/include/audio.h
@@ -4,6 +4,15 @@
 #include "structs.h"
 #include "datatypes.h"
 
+typedef enum FGMGroup
+{
+    FGMGROUP_PERSIST,   // main, pokemon, nr_name, end
+    FGMGROUP_ANNOUNCER, // nr_title, nr_select, nr_1p, nr_vs
+    FGMGROUP_CHARACTER, // character ssms + null.ssm
+    FGMGROUP_STAGE,     // stage ssms + last.ssm
+    FGMGROUP_1P,        // ending, 1pend
+} FGMGroup;
+
 char *Nametag_GetText(int tag_index);
 void Audio_ResetCache(int group_index);
 void Audio_QueueFileLoad(int group_index, u64 ssm_index);
