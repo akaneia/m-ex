77e53acfe4fbe68c2154fe1aa3bb7c18353c2f63
UnclePunch <vinbertinelli@gmail.com>
2021-12-22 14:21:29 -0500

    mainmenu: fix bug when returning from title screen
    use topmost menu when returning from unknown vanilla scenes

--- asm/m-ex/MainMenu Expansion/ReturnFromScene.asm
+++ asm/m-ex/MainMenu Expansion/ReturnFromScene.asm
@@ -2,6 +2,18 @@
 .include "../../Globals.s"
 .include "../Header.s"
 
-# this injection runs when a non vanilla scene returns to the main menu
+# check for a vanilla scene (use main menu)\
+  branchl r12,0x801a4320
+  cmpwi r3,44
+  ble isVanilla
+
+# this code runs when a non vanilla scene returns to the main menu
 # storing -1 to indicate to "Load MxMn.asm" to adjust this value
   li r0,-1
+  b Exit
+
+isVanilla:
+# use menu 0 if coming from a vanilla scene
+  li r0,0
+
+Exit:
