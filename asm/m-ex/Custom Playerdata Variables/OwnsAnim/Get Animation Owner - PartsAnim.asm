b0120024f6b3219faeb78834d0d2db7f288a1229
UnclePunch <vinbertinelli@gmail.com>
2021-12-30 07:02:29 -0500

    m-ex: fix thrown bone bug for hand anims

--- asm/m-ex/Custom Playerdata Variables/OwnsAnim/Get Animation Owner - PartsAnim.asm
+++ asm/m-ex/Custom Playerdata Variables/OwnsAnim/Get Animation Owner - PartsAnim.asm
@@ -4,5 +4,20 @@
 
 .set  REG_FighterData,22
 
+#Check if anim is coming from opponent
+  lwz r0,MEX_AnimOwner(REG_FighterData)
+  cmpwi r0,0
+  beq UseFtKind
+
+# Anim comes from opponent, so use ftkind num so 
+# we use the last entry in the PlCo bone table 
+# (thrown anim skeleton additional bones @ 0x14)
+  lwz r29,OFST_Metadata_FtIntNum(rtoc)
+  addi r29,r29,1
+  b Exit
+
+UseFtKind:
 # get ft_kind from fighterdata instead of the animation flags
   lwz r29, 0x4(REG_FighterData)
+
+Exit:
