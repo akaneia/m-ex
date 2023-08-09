c708afbb78d10420059cd8c1e7d1997e30497025
UnclePunch <vinbertinelli@gmail.com>
2023-08-09 00:54:22 -0400

    fix 328th stage not preloading
    melee uses 328 as the "null" id for stages, this changes it to 0xFFFFFFFF

---
+++ asm/m-ex/SSS Expansion/External Null ID/Check Null ID.asm
@@ -0,0 +1,5 @@
+#To be inserted @ 8001799c
+.include "../../../Globals.s"
+.include "../../Header.s"
+
+cmpwi	r3, -1
---
+++ asm/m-ex/SSS Expansion/External Null ID/Default Null ID.asm
@@ -0,0 +1,5 @@
+#To be inserted @ 803ba644
+.include "../../../Globals.s"
+.include "../../Header.s"
+
+.long -1
