6b900070c664d2aefd29fa9a621726c9155f50a8
Ploaj <ploaj_the_otter@yahoo.com>
2025-03-24 23:50:21 -0500

    added item visibility callback hooks

---
+++ asm/m-ex/Fighter OnItemVisibility/onSetItemInvisible.asm
@@ -0,0 +1,7 @@
+#To be inserted @ 8007f61c
+.include "../../Globals.s"
+.include "../Header.s"
+
+lwz  r4,OFST_mexData(rtoc)
+lwz  r4,Arch_FighterFunc(r4)
+lwz  r0,Arch_FighterFunc_onMakeItemInvisible(r4)
