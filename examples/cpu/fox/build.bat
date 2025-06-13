ebdceefb7fc30a731c58d0e007bb21b059079052
Ploaj <ploaj_the_otter@yahoo.com>
2025-06-12 19:13:24 -0500

    added sword trail example

--- examples/cpu/fox/build.bat
+++
@@ -1,3 +0,0 @@
-
-:: compile custom fighter function for fox
-"../../../MexTK/MexTK.exe" -ff -i "fox.c" -d -op 1 -s ftFunction -dat "PlFx.dat" -ow 
---
+++ examples/swordtrail/build.bat
@@ -0,0 +1,3 @@
+
+:: compile custom sword trail for marth
+"../../MexTK/MexTK.exe" -ff -i "marth.c" -d -op 1 -s ftFunction -dat "PlMs.dat" -ow 
