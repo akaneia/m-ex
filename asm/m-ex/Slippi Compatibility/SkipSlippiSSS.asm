7633a5bf5eec58579dabba7db82a0da82a34907e
KELLZ <forestruns@gmail.com>
2025-08-13 03:02:47 -0230

    simplify logic

--- asm/m-ex/Slippi Compatibility/SkipSlippiSSS.asm
+++ asm/m-ex/Slippi Compatibility/SkipSlippiSSS.asm
@@ -8,12 +8,7 @@
 .set STRNCMP, 0x803258a8
 .set Archive_GetLocalizedFilename, 0x80016204
 .set REG_FILENAME, 30
-
-b CODE_START
-
-STRING_BLRL:
-blrl
-.string "MnSlMap"
+.set REG_LR, 20
 
 CODE_START:
   branchl r12, Archive_GetLocalizedFilename # replaced branch
@@ -25,21 +20,22 @@ CODE_START:
   lwz r3,0x0(r3)
   load r4,0x40820010
   cmpw r3,r4
-  bne EXIT
+  beq EXIT
 
-# if not SSS, continue as normal
-  bl STRING_BLRL
-  mflr r3
-  mr r4, r3
-  li r5, 7
-  branchl r12, STRNCMP
-  cmpwi r3, 0
-  bne EXIT
+# check if we're coming from the SSS
+  lwz r0, 0x4(sp)
+  load r3, 0x8025a9dc
+  cmpw r0, r3
+  beq SKIP_SSS
+  load r3, 0x8025a9ec
+  cmpw r0, r3
+  beq SKIP_SSS
+  b EXIT
 
-# skip Slippi SSS
-  restore
-  branch r12, 0x800166bc
+  SKIP_SSS:
+    restore
+    branch r12, 0x800166bc
 
 EXIT:
-  mr r3, REG_FILENAME
   restore
+  mr r3, REG_FILENAME
